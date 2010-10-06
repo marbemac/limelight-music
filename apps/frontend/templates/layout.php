8d6fd4cd143ceacef17397b63d3163536b0631b9
marc <marbemac@gmail.com>
2010-10-05 23:28:22 -0400

    - updated new api keys
    Signed-off-by: marc <marbemac@gmail.com>

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -194,7 +194,7 @@
     </script>
 
     <script type="text/javascript">
-      RPXNOW.init({appId: 'mpibekcdcbdablmnaeoh',
+      RPXNOW.init({appId: 'gmgpjfkdifpfajnignih',
         xdReceiver: '/rpx_xdcomm.html'});
     </script>
     <!-- end RPX publishing -->
@@ -219,18 +219,13 @@
     
     <!-- GOOGLE ANALYTICS -->
     <script type="text/javascript">
+      var rpxJsHost = (("https:" == document.location.protocol) ? "https://" : "http://static.");
+      document.write(unescape("%3Cscript src='" + rpxJsHost + "rpxnow.com/js/lib/rpx.js' type='text/javascript'%3E%3C/script%3E"));
+    </script>
 
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-6788018-1']);
-      _gaq.push(['_setDomainName', '.techlimelight.com']);
-      _gaq.push(['_trackPageview']);
-
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-
+    <script type="text/javascript">
+      RPXNOW.init({appId: 'gmgpjfkdifpfajnignih',
+        xdReceiver: '/rpx_xdcomm.html'});
     </script>
     <!-- END GOOGLE -->
   </body>
